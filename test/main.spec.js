46aff60a48c3ecc010bcae50e96b5ad7ae0afb60
Hangur <44006666+Hangur@users.noreply.github.com>
2020-05-30 22:23:27 +0200

    Added tests for idInvalidCardCompanies

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,21 +1,15 @@
 import { expect } from 'chai';
 import Validator from '../main';
 
-// Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
-
-
 const testInputs = {
     valid: [
         [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8],
         [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9],
         [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6],
         [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5],
-        [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+        [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6],
+        [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9],
+        [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
     ],
     invalid: [
         [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5],
@@ -23,7 +17,10 @@ const testInputs = {
         [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4],
         [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5],
         [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4],
-        [7, 8, 9, 1, 1, 2, 7, 9, 7, 1, 0, 9, 1]
+        [7, 8, 9, 1, 1, 2, 7, 9, 7, 1, 0, 9, 1],
+        [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4],
+        [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3],
+        [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
     ]
 };
 
@@ -54,6 +51,14 @@ describe('validateCreditCard', () => {
     
         it('valid scenario 5', () => {
             validateValid(testInputs.valid[4]);
+        });
+
+        it('valid scenario 6', () => {
+            validateValid(testInputs.valid[5]);
+        });
+
+        it('valid scenario 7', () => {
+            validateValid(testInputs.valid[6]);
         });
     });
     
@@ -86,6 +91,18 @@ describe('validateCreditCard', () => {
 
         it('invalid scenario 6', () => {
             validateInvalid(testInputs.invalid[5]);
+        });
+
+        it('invalid scenario 7', () => {
+            validateInvalid(testInputs.invalid[6]);
+        });
+
+        it('invalid scenario 8', () => {
+            validateInvalid(testInputs.invalid[7]);
+        });
+
+        it('invalid scenario 9', () => {
+            validateInvalid(testInputs.invalid[8]);
         });
     });
 });
@@ -136,4 +153,46 @@ describe('findInvalidCards', () => {
         expect(result).to.deep.equal(expected);
     });
 
+});
+
+describe('idInvalidCardCompanies', () => {
+
+    it('finds IDs of companies with invalid cards', () => {
+        
+        const batchOfInvalidCards = [
+            testInputs.invalid[6],
+            testInputs.invalid[7],
+            testInputs.invalid[8]
+        ];
+        const expected = ['Amex', 'Discover', 'Visa'];
+        const result = Validator.idInvalidCardCompanies(batchOfInvalidCards);
+
+        expect(result).to.deep.equal(expected);
+
+    });
+
+    it('returns only unique occurences', () => {
+
+        const batchOfInvalidCards = [
+            testInputs.invalid[0],
+            testInputs.invalid[1],
+            testInputs.invalid[8]
+        ];
+        const expected = ['Visa', 'Mastercard'];
+        const result = Validator.idInvalidCardCompanies(batchOfInvalidCards);
+
+        expect(result).to.deep.equal(expected);
+    });
+
+    it('throws an error on uknown card company', () => {
+
+        const batchOfInvalidCards = [
+            testInputs.invalid[5],
+            testInputs.invalid[6]
+        ];
+        
+        expect(() => {
+            Validator.idInvalidCardCompanies(batchOfInvalidCards);
+        }).to.throw(Error, 'Company not found!');
+    });
 });
