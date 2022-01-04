644f07e6fad6727607b84d3a9c22d3cb2f0b861d
han <44006666+Hangur@users.noreply.github.com>
2022-01-04 19:26:25 +0100

    Formatted and added jest

--- main.js
+++ main.js
@@ -1,73 +1,73 @@
-const validateCreditCard = (array) => {
-    let multiplyByTwo = false;
-    const register = [];
+const validateCreditCard = array => {
+  let multiplyByTwo = false
+  const register = []
 
-    for (let i = array.length -1; i >= 0; i--) {
-        if (!multiplyByTwo) {
-            register.push(array[i]);
-        } else {
-            const tempNumber = array[i] * 2;
-            if (tempNumber > 9) {
-                register.push(tempNumber - 9);
-            } else {
-                register.push(tempNumber);
-            }
-        }
-        multiplyByTwo = !multiplyByTwo;
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (!multiplyByTwo) {
+      register.push(array[i])
+    } else {
+      const tempNumber = array[i] * 2
+      if (tempNumber > 9) {
+        register.push(tempNumber - 9)
+      } else {
+        register.push(tempNumber)
+      }
     }
+    multiplyByTwo = !multiplyByTwo
+  }
 
-    let sum = 0;
-    for (let i = 0; i < register.length; i++) {
-        sum = sum + register[i];
+  let sum = 0
+  for (let i = 0; i < register.length; i++) {
+    sum = sum + register[i]
+  }
+  const result = sum % 10
+  return result === 0
+}
+
+const findInvalidCards = batch => {
+  if (typeof batch === 'undefined') {
+    return []
+  }
+  const batchOfInvalid = []
+  for (let i = 0; i < batch.length; i++) {
+    const currentCreditCard = batch[i]
+    const result = validateCreditCard(currentCreditCard)
+    if (!result) {
+      batchOfInvalid.push(currentCreditCard)
     }
-    const result = sum % 10;
-    return result === 0;
-};
+  }
+  return batchOfInvalid
+}
 
-const findInvalidCards = (batch) => {
-    if (typeof batch === 'undefined') {
-        return [];
+const idInvalidCardCompanies = batchOfInvalid => {
+  const creditCardCompanies = {
+    3: 'Amex',
+    4: 'Visa',
+    5: 'Mastercard',
+    6: 'Discover'
+  }
+
+  const companies = []
+
+  for (let i = 0; i < batchOfInvalid.length; i++) {
+    const currentInvalidCard = batchOfInvalid[i]
+    const firstNumber = currentInvalidCard[0]
+    const company = creditCardCompanies[firstNumber]
+    if (typeof company === 'undefined') {
+      throw new Error('Company not found!')
+    } else {
+      if (!companies.includes(company)) {
+        companies.push(company)
+      }
     }
-    const batchOfInvalid = [];
-    for (let i = 0; i < batch.length; i++) {
-        const currentCreditCard = batch[i];
-        const result = validateCreditCard(currentCreditCard);
-        if (!result) {
-            batchOfInvalid.push(currentCreditCard);
-        }
-    }
-    return batchOfInvalid;
-};
-
-const idInvalidCardCompanies = (batchOfInvalid) => {
-    const creditCardCompanies = {
-        3: 'Amex',
-        4: 'Visa',
-        5: 'Mastercard',
-        6: 'Discover'
-    };
-
-    const companies = [];
-
-    for (let i = 0; i < batchOfInvalid.length; i++) {
-        const currentInvalidCard = batchOfInvalid[i];
-        const firstNumber = currentInvalidCard[0];
-        const company = creditCardCompanies[firstNumber];
-        if (typeof company === 'undefined') {
-            throw new Error('Company not found!');
-        } else {
-            if (!companies.includes(company)) {
-                companies.push(company);
-            }
-        }     
-    }
-    return companies;
-};
+  }
+  return companies
+}
 
 const Validator = {
-    validateCreditCard: validateCreditCard,
-    findInvalidCards: findInvalidCards,
-    idInvalidCardCompanies: idInvalidCardCompanies
-};
+  validateCreditCard: validateCreditCard,
+  findInvalidCards: findInvalidCards,
+  idInvalidCardCompanies: idInvalidCardCompanies
+}
 
-export default Validator;
+export default Validator
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,198 +1,171 @@
-import { expect } from 'chai';
-import Validator from '../main';
+import Validator from '../main'
 
 const testInputs = {
-    valid: [
-        [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8],
-        [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9],
-        [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6],
-        [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5],
-        [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6],
-        [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9],
-        [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
-    ],
-    invalid: [
-        [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5],
-        [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3],
-        [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4],
-        [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5],
-        [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4],
-        [7, 8, 9, 1, 1, 2, 7, 9, 7, 1, 0, 9, 1],
-        [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4],
-        [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3],
-        [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
-    ]
-};
+  valid: [
+    [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8],
+    [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9],
+    [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6],
+    [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5],
+    [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6],
+    [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9],
+    [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+  ],
+  invalid: [
+    [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5],
+    [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3],
+    [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4],
+    [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5],
+    [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4],
+    [7, 8, 9, 1, 1, 2, 7, 9, 7, 1, 0, 9, 1],
+    [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4],
+    [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3],
+    [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
+  ]
+}
 
 describe('validateCreditCard', () => {
-    
-    describe('valid credit cards', () => {
+  describe('valid credit cards', () => {
+    const validateValid = validCreditCard => {
+      const result = Validator.validateCreditCard(validCreditCard)
+      expect(result).toBe(true)
+    }
 
-        const validateValid = (validCreditCard) => {
-            const result = Validator.validateCreditCard(validCreditCard);
-            expect(result).to.be.true;
-        };
+    it('valid scenario 1', () => {
+      validateValid(testInputs.valid[0])
+    })
 
-        it('valid scenario 1', () => {
-            validateValid(testInputs.valid[0]);
-        });
-    
-        it('valid scenario 2', () => {
-            validateValid(testInputs.valid[1]);
-        });
-    
-        it('valid scenario 3', () => {
-            validateValid(testInputs.valid[2]);
-        });
-    
-        it('valid scenario 4', () => {
-            validateValid(testInputs.valid[3]);
-        });
-    
-        it('valid scenario 5', () => {
-            validateValid(testInputs.valid[4]);
-        });
+    it('valid scenario 2', () => {
+      validateValid(testInputs.valid[1])
+    })
 
-        it('valid scenario 6', () => {
-            validateValid(testInputs.valid[5]);
-        });
+    it('valid scenario 3', () => {
+      validateValid(testInputs.valid[2])
+    })
 
-        it('valid scenario 7', () => {
-            validateValid(testInputs.valid[6]);
-        });
-    });
-    
-    describe('invalid credit cards', () => {
+    it('valid scenario 4', () => {
+      validateValid(testInputs.valid[3])
+    })
 
-        const validateInvalid = (invalidCreditCard) => {
-            const result = Validator.validateCreditCard(invalidCreditCard);
-            expect(result).to.be.false;
-        };
+    it('valid scenario 5', () => {
+      validateValid(testInputs.valid[4])
+    })
 
-        it('invalid scenario 1', () => {
-            validateInvalid(testInputs.invalid[0]);
-        });
-    
-        it('invalid scenario 2', () => {
-            validateInvalid(testInputs.invalid[1]);
-        });
-    
-        it('invalid scenario 3', () => {
-            validateInvalid(testInputs.invalid[2]);
-        });
-    
-        it('invalid scenario 4', () => {
-            validateInvalid(testInputs.invalid[3]);
-        });
-    
-        it('invalid scenario 5', () => {
-            validateInvalid(testInputs.invalid[4]);
-        });
+    it('valid scenario 6', () => {
+      validateValid(testInputs.valid[5])
+    })
 
-        it('invalid scenario 6', () => {
-            validateInvalid(testInputs.invalid[5]);
-        });
+    it('valid scenario 7', () => {
+      validateValid(testInputs.valid[6])
+    })
+  })
 
-        it('invalid scenario 7', () => {
-            validateInvalid(testInputs.invalid[6]);
-        });
+  describe('invalid credit cards', () => {
+    const validateInvalid = invalidCreditCard => {
+      const result = Validator.validateCreditCard(invalidCreditCard)
+      expect(result).toBe(false)
+    }
 
-        it('invalid scenario 8', () => {
-            validateInvalid(testInputs.invalid[7]);
-        });
+    it('invalid scenario 1', () => {
+      validateInvalid(testInputs.invalid[0])
+    })
 
-        it('invalid scenario 9', () => {
-            validateInvalid(testInputs.invalid[8]);
-        });
-    });
-});
+    it('invalid scenario 2', () => {
+      validateInvalid(testInputs.invalid[1])
+    })
+
+    it('invalid scenario 3', () => {
+      validateInvalid(testInputs.invalid[2])
+    })
+
+    it('invalid scenario 4', () => {
+      validateInvalid(testInputs.invalid[3])
+    })
+
+    it('invalid scenario 5', () => {
+      validateInvalid(testInputs.invalid[4])
+    })
+
+    it('invalid scenario 6', () => {
+      validateInvalid(testInputs.invalid[5])
+    })
+
+    it('invalid scenario 7', () => {
+      validateInvalid(testInputs.invalid[6])
+    })
+
+    it('invalid scenario 8', () => {
+      validateInvalid(testInputs.invalid[7])
+    })
+
+    it('invalid scenario 9', () => {
+      validateInvalid(testInputs.invalid[8])
+    })
+  })
+})
 
 describe('findInvalidCards', () => {
+  it('finds invalid cards', () => {
+    const batchOfCards = [testInputs.valid[0], testInputs.valid[1], testInputs.invalid[5]]
+    const expected = [testInputs.invalid[5]]
+    const result = Validator.findInvalidCards(batchOfCards)
 
-    it('finds invalid cards', () => {
+    expect(result).toEqual(expected)
+  })
 
-        const batchOfCards = [
-            testInputs.valid[0],
-            testInputs.valid[1],
-            testInputs.invalid[5]
-        ];
-        const expected = [
-            testInputs.invalid[5]
-        ];
-        const result = Validator.findInvalidCards(batchOfCards);
+  it('returns no invalid cards when there are no invalid', () => {
+    const batchOfCards = [testInputs.valid[2], testInputs.valid[3]]
+    const expected = []
+    const result = Validator.findInvalidCards(batchOfCards)
 
-        expect(result).to.deep.equal(expected);
-    });
+    expect(result).toEqual(expected)
+  })
 
-    it('returns no invalid cards when there are no invalid', () => {
-        
-        const batchOfCards = [
-            testInputs.valid[2], 
-            testInputs.valid[3]
-        ];
-        const expected = [];
-        const result = Validator.findInvalidCards(batchOfCards);
+  it('returns no invalid cards when there is an empty batch', () => {
+    const batchOfCards = []
+    const expected = []
+    const result = Validator.findInvalidCards(batchOfCards)
 
-        expect(result).to.deep.equal(expected);
-    });
+    expect(result).toEqual(expected)
+  })
 
-    it('returns no invalid cards when there is an empty batch', () => {
+  it('returns no invalid cards when there is no input', () => {
+    const expected = []
+    const result = Validator.findInvalidCards()
 
-        const batchOfCards = [];
-        const expected = [];
-        const result = Validator.findInvalidCards(batchOfCards);
-
-        expect(result).to.deep.equal(expected);
-    });
-
-    it('returns no invalid cards when there is no input', () => {
-
-        const expected = [];
-        const result = Validator.findInvalidCards();
-
-        expect(result).to.deep.equal(expected);
-    });
-
-});
+    expect(result).toEqual(expected)
+  })
+})
 
 describe('idInvalidCardCompanies', () => {
+  it('finds IDs of companies with invalid cards', () => {
+    const batchOfInvalidCards = [
+      testInputs.invalid[6],
+      testInputs.invalid[7],
+      testInputs.invalid[8]
+    ]
+    const expected = ['Amex', 'Discover', 'Visa']
+    const result = Validator.idInvalidCardCompanies(batchOfInvalidCards)
 
-    it('finds IDs of companies with invalid cards', () => {
-        
-        const batchOfInvalidCards = [
-            testInputs.invalid[6],
-            testInputs.invalid[7],
-            testInputs.invalid[8]
-        ];
-        const expected = ['Amex', 'Discover', 'Visa'];
-        const result = Validator.idInvalidCardCompanies(batchOfInvalidCards);
+    expect(result).toEqual(expected)
+  })
 
-        expect(result).to.deep.equal(expected);
+  it('returns only unique occurences', () => {
+    const batchOfInvalidCards = [
+      testInputs.invalid[0],
+      testInputs.invalid[1],
+      testInputs.invalid[8]
+    ]
+    const expected = ['Visa', 'Mastercard']
+    const result = Validator.idInvalidCardCompanies(batchOfInvalidCards)
 
-    });
+    expect(result).toEqual(expected)
+  })
 
-    it('returns only unique occurences', () => {
+  it('throws an error on uknown card company', () => {
+    const batchOfInvalidCards = [testInputs.invalid[5], testInputs.invalid[6]]
 
-        const batchOfInvalidCards = [
-            testInputs.invalid[0],
-            testInputs.invalid[1],
-            testInputs.invalid[8]
-        ];
-        const expected = ['Visa', 'Mastercard'];
-        const result = Validator.idInvalidCardCompanies(batchOfInvalidCards);
-
-        expect(result).to.deep.equal(expected);
-    });
-
-    it('throws an error on uknown card company', () => {
-
-        const batchOfInvalidCards = [
-            testInputs.invalid[5],
-            testInputs.invalid[6]
-        ];
-        
-        expect(() => {
-            Validator.idInvalidCardCompanies(batchOfInvalidCards);
-        }).to.throw(Error, 'Company not found!');
-    });
-});
+    expect(() => {
+      Validator.idInvalidCardCompanies(batchOfInvalidCards)
+    }).toThrow(Error, 'Company not found!')
+  })
+})
